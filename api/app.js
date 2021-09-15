131832e1a5665ae3665dbafe45eaad605965c68e
Shikhar15606 <shikharshukla15606@gmail.com>
2021-09-15 21:41:35 +0530

    API Integration with a test route

---
+++ api/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const logger = require("morgan");
+
+require("dotenv").config();
+
+var app = express();
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api", require("./routes/api"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server Running at ${port}`)
+});
+
+module.exports = app;
