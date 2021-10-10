054620276f9af75b108a0d6c9b39ff73fc3cac3a
Abhishek357 <abhisheksinghchauhan357@gmail.com>
2021-10-10 14:21:53 +0530

    added leader board
    Signed-off-by: Abhishek357 <abhisheksinghchauhan357@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 // import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./getWeb3";
 import Navbar from './components/Navbar';
+import Leaderboard from './components/Leaderboard';
 import "./App.css";
 
 class App extends Component {
@@ -53,19 +54,24 @@ class App extends Component {
     //   return <div>Loading Web3, accounts, and contract...</div>;
     // }
     return (
-      <div className="App">
-        <Navbar />
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        {/* <div>The stored value is: {this.state.storageValue}</div> */}
+      <div>
+        <div className="App">
+          <Navbar />
+          {/* <h1>Good to Go!</h1>
+          <p>Your Truffle Box is installed and ready.</p>
+          <h2>Smart Contract Example</h2>
+          <p>
+            If your contracts compiled and migrated successfully, below will show
+            a stored value of 5 (by default).
+          </p>
+          <p>
+            Try changing the value stored on <strong>line 42</strong> of App.js.
+          </p> */}
+          {/* <div>The stored value is: {this.state.storageValue}</div> */}
+        </div>
+        <div class="p-8 ...">
+            <Leaderboard />
+        </div>        
       </div>
     );
   }
