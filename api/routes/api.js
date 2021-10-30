1cf60a983ff307bfa27717ae7b44e7c9873ef92f
Abhishek357 <abhisheksinghchauhan357@gmail.com>
2021-10-30 09:05:52 +0530

    added sendOTP API

--- api/routes/api.js
+++ api/routes/api.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 // const Provider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+const { fetchVoter } = require('../middlewares/auth');
 
 // const privateKey = process.env.PRIVATEKEY;
 // const url = process.env.BLOCKCHAINURL;
 // const address = process.env.ADDRESS;
+const client = require('twilio')(
+  process.env.ACCOUNT_SID,
+  process.env.AUTH_TOKEN
+);
 
 router.get('/', (req, res, next) => res.json({ msg: 'Application Running' }));
 
@@ -27,6 +33,45 @@ router.get('/test/', async (req, res, next) => {
   //   console.log(err);
   // }
   res.send('Hi');
+});
+
+// Register Voter in Blockchain
+// Send OTP to Voter
+router.get('/regVoter/sendOTP', fetchVoter, async (req, res, next) => {
+  console.log(req.phone);
+  console.log(req.pinCode);
+  console.log(req.hasRegistered);
+  if (req.hasRegistered === true) {
+    return res.status(400).json({
+      msg: 'Voter already registered',
+    });
+  }
+
+  //   console.log(req.phone);
+  //   console.log(req.pinCode);
+  //   console.log(req.hasRegistered);
+
+  if (req.phone) {
+    client.verify
+      .services(process.env.SERVICE_ID)
+      .verifications.create({
+        to: `+${req.phone}`,
+        channel: 'sms',
+      })
+      .then(data => {
+        res.status(200).send({
+          message: 'OTP is sent!',
+          phonenumber: req.phone,
+          data,
+        });
+      });
+  } else {
+    res.status(400).send({
+      message: 'Wrong phone number :(',
+      phonenumber: req.phone,
+      data,
+    });
+  }
 });
 
 module.exports = router;
