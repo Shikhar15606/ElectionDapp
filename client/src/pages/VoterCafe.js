04f33b8a590ab0f76c232e1a02a994cabeb1d6b8
Shikhar15606 <shikharshukla15606@gmail.com>
2021-11-22 21:09:34 +0530

    Bring back testing edits

--- client/src/pages/VoterCafe.js
+++ client/src/pages/VoterCafe.js
@@ -3,6 +3,7 @@ import Vote from '../components/Vote';
 import Leaderboard from '../components/Leaderboard';
 import MessageComponent from '../components/Message';
 import { getPhase } from '../actions/smartContract';
+import VoterRegistration from '../components/VoterRegistration';
 
 const VoterCafe = props => {
   const [phase, setPhase] = useState(1);
@@ -18,10 +19,10 @@ const VoterCafe = props => {
   }, [fetchData]);
 
   switch (phase) {
-    // case 1:
-    //   return <VoterRegistration />;
     case 1:
-      return <Leaderboard contract={props.contract} />;
+      return <VoterRegistration />;
+    // case 1:
+    //   return <Leaderboard contract={props.contract} />;
     case 2:
       return <Vote />;
     case 3:
