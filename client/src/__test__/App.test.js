8742d56bea69266b9a86072977f9bbd611e8a24b
hardik5062000 <hardik5062000@gmail.com>
2021-11-23 20:23:02 +0530

    App.js UnitTesting setup added

---
+++ client/src/__test__/App.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+
+import { render, screen } from '@testing-library/react';
+
+// "test" or "it" both are same - we use it syntax
+/* Test Block: Render component to test, find elements to interact
+    interact with those elements and assert that the result is as expected.
+*/
+
+it('Main App renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('Metamask prompt renders before App', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Loading Web3/i);
+  expect(linkElement).toBeInTheDocument();
+});
