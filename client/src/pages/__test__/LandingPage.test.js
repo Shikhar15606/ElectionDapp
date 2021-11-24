35ed536bc55cd0db36bb2d1561a162d4e5ed8251
hardik5062000 <hardik5062000@gmail.com>
2021-11-24 10:18:10 +0530

    Fixed Route based Testing on LandingPage

--- client/src/pages/__test__/LandingPage.test.js
+++ client/src/pages/__test__/LandingPage.test.js
@@ -2,19 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LandingPage from '../LandingPage';
 
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
 // "test" or "it" both are same - we use it syntax
 /* Test Block: Render component to test, find elements to interact
     interact with those elements and assert that the result is as expected.
 */
 
-it('should render LandingPage successfully', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<LandingPage />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+const MockLandingPage = () => {
+  return (
+    <BrowserRouter>
+      <LandingPage />
+    </BrowserRouter>
+  );
+};
 
-// it("renders again",() => {
-//   render(<App/>);
-//   const linkElement = screen.getByText(/Loading Web3/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+it('should render LandingPage successfully', () => {
+  render(MockLandingPage);
+});
